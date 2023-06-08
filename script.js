2c7c015b6b5570e8f7152e89a20a68d63a04efd9
cristi <scarlat.cristian.gabriel@gmail.com>
2023-06-08 15:12:24 +0300

    added JS

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const range = document.getElementById("range");
+
+const scale = (number, inMin, inMax, outMin, outMax) => {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
+
+range.addEventListener("input", (e) => {
+  const value = +e.target.value;
+  const label = e.target.nextElementSibling;
+
+  const range_width = getComputedStyle(e.target).getPropertyValue("width");
+  const label_width = getComputedStyle(label).getPropertyValue("width");
+
+  //   taking the px off from range and making it a number
+  const num_width = +range_width.substring(0, range_width.length - 2);
+  const num_label_width = +label_width.substring(0, label_width.length - 2);
+
+  const max = +e.target.max;
+  const min = +e.target.min;
+
+  const left =
+    value * (num_width / max) -
+    num_label_width / 2 +
+    scale(value, min, max, 10, -10);
+
+  label.style.left = `${left}px`;
+  label.innerHTML = value;
+});
